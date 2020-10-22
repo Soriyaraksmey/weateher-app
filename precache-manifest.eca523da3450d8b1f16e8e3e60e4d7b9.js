c376072171bc77b99fe2f11721901015c53576ce
Soriyaraksmey <kohosoriya@gmail.com>
2020-10-22 09:21:08 +0700

    Updates

---
+++ precache-manifest.eca523da3450d8b1f16e8e3e60e4d7b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f9b66e9f41d8a8af17aba3c2d10012",
+    "url": "/weateher-app/index.html"
+  },
+  {
+    "revision": "22602254bcc3e13d63f1",
+    "url": "/weateher-app/static/css/main.71235b97.chunk.css"
+  },
+  {
+    "revision": "8370dffebb6ecc059221",
+    "url": "/weateher-app/static/js/2.69a16f7f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weateher-app/static/js/2.69a16f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22602254bcc3e13d63f1",
+    "url": "/weateher-app/static/js/main.ebd48f10.chunk.js"
+  },
+  {
+    "revision": "962d6b20430e1c1ff935",
+    "url": "/weateher-app/static/js/runtime-main.e361ce23.js"
+  },
+  {
+    "revision": "74e44b8d9099d2439416ddb2f3f2ac54",
+    "url": "/weateher-app/static/media/myedit.74e44b8d.mp4"
+  }
+]);
